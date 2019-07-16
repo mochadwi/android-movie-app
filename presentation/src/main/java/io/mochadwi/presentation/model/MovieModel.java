a8400b5684164e8691660480236fb8ecdd410d3f
mochadwi <moch.iqbal@dana.id>
2019-07-16 16:39:30 +0700

    refactor(presentation): Annotate the Movie model
    Remove boilerplate for movie pojo to generate getter & setter

--- presentation/src/main/java/io/mochadwi/presentation/model/MovieModel.java
+++ presentation/src/main/java/io/mochadwi/presentation/model/MovieModel.java
@@ -1,8 +1,11 @@
 package io.mochadwi.presentation.model;
 
+import lombok.Data;
+
 /**
  * Class that represents a user in the presentation layer.
  */
+@Data
 public class MovieModel {
 
     private final int userId;
@@ -16,52 +19,4 @@ public class MovieModel {
     private int followers;
 
     private String fullName;
-
-    public MovieModel(int userId) {
-        this.userId = userId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getCoverUrl() {
-        return coverUrl;
-    }
-
-    public void setCoverUrl(String coverUrl) {
-        this.coverUrl = coverUrl;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(int followers) {
-        this.followers = followers;
-    }
 }
